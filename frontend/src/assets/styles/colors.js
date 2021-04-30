fa2501b01d486a5f235855e74c3ba2d9df55c228
patrick-moreira <patrickmoreira1995@gmail.com>
2021-04-30 18:08:11 -0300

    Estruturaçao de arquivos e pastas do projeto

---
+++ frontend/src/assets/styles/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: '#151726',
+  secundary: '#E1E1E1',
+  tertiary: '#CCCCCC',
+  quaternary: '#333333',
+
+  info: '#36679E',
+  success: '#00c57e',
+  failure: '##ff5869',
+};
---
+++ frontend/src/assets/styles/metrics.js
@@ -0,0 +1,16 @@
+export default {
+  space: {
+    smaller: 0.5,
+    small: 1,
+    medium: 1.5,
+    big: 2,
+    bigger: 2.5,
+  },
+  font: {
+    smaller: 1.2,
+    small: 1.6,
+    medium: 2.2,
+    big: 2.8,
+    bigger: 3.2,
+  },
+};
