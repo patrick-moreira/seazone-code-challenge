bf7464c333c85322d12aff3d0f2ca85723aa2d33
patrick-moreira <patrickmoreira1995@gmail.com>
2021-05-04 18:27:14 -0300

    Padronizaçao das metricas de font e espaçamento

--- frontend/src/assets/styles/metrics.js
+++ frontend/src/assets/styles/metrics.js
@@ -6,11 +6,20 @@ export default {
     big: 2,
     bigger: 2.5,
   },
-  font: {
-    smaller: 1.2,
-    small: 1.6,
-    medium: 2.2,
+  font_size: {
+    tiny: 0.8,
+    smaller: 1,
+    small: 1.2,
+    regular: 1.4,
+    medium: 1.6,
+    larger: 1.8,
+    xlarger: 2.0,
     big: 2.8,
-    bigger: 3.2,
+    bigger: 3.6,
+    huge: 5.2,
+  },
+  font_weight: {
+    normal: 400,
+    bold: 700,
   },
 };
