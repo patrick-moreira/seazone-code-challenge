74fb7a9b1e59b9fc51583dbc95299ffa0717a05e
patrick-moreira <patrickmoreira1995@gmail.com>
2021-05-06 12:55:44 -0300

    ref: Pagina de detalhes dos imoveis refatorada

--- frontend/src/pages/Details/index.js
+++ frontend/src/pages/Details/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { MdArrowDropDown } from 'react-icons/md';
 import { FiUsers, FiTrash, FiDollarSign } from 'react-icons/fi';
 
@@ -8,213 +8,98 @@ import Filter from '../../components/Filter';
 import {
   Container,
   Content,
-  Title,
+  WrapperHeader,
   WrapperLocation,
   WrapperImovel,
-  WrapperRules,
   WrapperComforts,
-  DivStyled,
   StrongStyled,
 } from './styles';
 import img from '../../assets/images/imoveis/1.png';
 
 function Details() {
+  const location = useLocation();
+  const { data } = location.state;
+
   return (
     <>
       <Filter />
       <Container className="container">
         <Content>
-          <Title>
-            <Link to="/propriedades">
-              <button type="button">Imóveis </button>
-            </Link>
-            <strong> &gt; Jurerê Internacional</strong>
-          </Title>
+          <WrapperHeader status={data.status}>
+            <div>
+              <button type="button">
+                <Link to="/propriedades">Imóveis</Link>
+              </button>
+              <h5> &nbsp; &gt; &nbsp; {data.name}</h5>
+            </div>
+            <div>
+              <div />
+              <strong>{data.status}</strong>
+            </div>
+          </WrapperHeader>
 
           <WrapperLocation>
-            <img src={img} alt="Imóvel 1" />
+            <img src={img} alt={data.name} />
             <div>
               <h4>Localização</h4>
               <p>
-                AV. Búzios 1800, Jurerê Internacional, Florianópolis - SC . CEP
-                89650-100
+                {data.address.lagradouro} {data.address.number},{' '}
+                {data.address.district}, {data.address.citie} -{' '}
+                {data.address.uf} . CEP {data.address.cep}
               </p>
               <button type="button">Ver localização</button>
             </div>
           </WrapperLocation>
 
           <WrapperImovel>
-            <h3>ILC2307 - Suite Top vista mar resort Jurere</h3>
-            <div id="info-container">
-              <div>
-                <p>Jurerê Internacional, Florianópolis</p>
-                <strong>2 quartos. 3 camas. 2 banheiros</strong>
-              </div>
-              <DivStyled status="active">
-                <div />
-                <strong>ativo</strong>
-              </DivStyled>
-            </div>
+            <h3>{data.name}</h3>
+            <p>{data.description}</p>
 
             <div id="cards-container">
               <div className="cards-content">
                 <strong>Máx. de hóspedes</strong>
                 <div>
                   <FiUsers size={30} />
-                  <strong>6</strong>
+                  <strong>{data.max_guests}</strong>
                 </div>
               </div>
               <div className="cards-content">
                 <strong>Taxa de limpeza</strong>
                 <div>
                   <FiTrash size={30} />
-                  <strong>R$50,00</strong>
+                  <strong>
+                    R$
+                    {data.cleaning_fee.toFixed(2).toString().replace('.', ',')}
+                  </strong>
                 </div>
               </div>
               <div className="cards-content">
                 <strong>Caução</strong>
                 <div>
                   <FiDollarSign size={30} />
-                  <strong>R$700,00</strong>
+                  <strong>
+                    R${data.caucao.toFixed(2).toString().replace('.', ',')}
+                  </strong>
                 </div>
               </div>
             </div>
-
-            <div id="beds-container">
-              <h4>Camas</h4>
-              <ul>
-                <li>
-                  <p>
-                    <strong>1 cama de casal: </strong>duplo
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <strong>1 sofá cama: </strong>sala/área comum
-                  </p>
-                </li>
-              </ul>
-            </div>
           </WrapperImovel>
-
-          <WrapperRules>
-            <h4>Regras</h4>
-            <div id="rules-container">
-              <ul>
-                <li>
-                  <p>
-                    Aceita Crianças (de 02 até 12 anos)
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Aceita Bebês (abaixo de 02 anos)
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Fornece Berços
-                    <StrongStyled status="não">não</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Restrição de Idade Mínima para Fazer Reserva
-                    <StrongStyled status="não">não</StrongStyled>
-                  </p>
-                </li>
-              </ul>
-
-              <ul>
-                <li>
-                  <p>
-                    Permitido Fumar no Quarto
-                    <StrongStyled status="não">não</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Aceita animais de Estimação
-                    <StrongStyled status="não">não</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Eventos são Permitidos
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-              </ul>
-            </div>
-            <button type="button">
-              Mais regras <MdArrowDropDown size={30} />
-            </button>
-          </WrapperRules>
 
           <WrapperComforts>
             <h4>Comodidades</h4>
 
-            <div id="comforts-container">
-              <ul>
-                <li>
+            <ul>
+              {data.comforts.map((item) => (
+                <li key={item}>
                   <p>
-                    Estacionamento
-                    <StrongStyled status="não">sim</StrongStyled>
+                    {item.split('=')[0]}
+                    <StrongStyled hasComfort={item.split('=')[1]}>
+                      {item.split('=')[1]}
+                    </StrongStyled>
                   </p>
                 </li>
-
-                <li>
-                  <p>
-                    Elevador
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Porteiro
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Piscina Privada
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-              </ul>
-
-              <ul>
-                <li>
-                  <p>
-                    Internet
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Wi-fi
-                    <StrongStyled status="sim">sim</StrongStyled>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Ar Condicionado
-                    <StrongStyled status="não">não</StrongStyled>
-                  </p>
-                </li>
-              </ul>
-            </div>
+              ))}
+            </ul>
 
             <button type="button">
               Mais comodidades <MdArrowDropDown size={30} />
--- frontend/src/pages/Details/styles.js
+++ frontend/src/pages/Details/styles.js
@@ -10,7 +10,6 @@ export const Content = styled.div`
   grid-template-areas:
     'header_ header_'
     'location info'
-    'location rules'
     'location comforts';
   column-gap: ${metrics.space.big * 2}rem;
 
@@ -20,16 +19,19 @@ export const Content = styled.div`
   p,
   strong,
   button {
-    font-size: ${metrics.font_size.regular}rem;
+    font-size: ${metrics.font_size.medium - 0.1}rem;
   }
   button {
     font-weight: ${metrics.font_weight.bold};
-  }
-  button:not(:first-child) {
-    color: ${colors.red};
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    :first-child {
+      margin-bottom: ${metrics.space.smaller}rem;
+    }
+    :not(:first-child) {
+      color: ${colors.red};
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
 
   @media (max-width: 800px) {
@@ -37,22 +39,46 @@ export const Content = styled.div`
       'header_ header_'
       'location location'
       'info info'
-      'rules rules'
       'comforts comforts';
   }
 `;
 
-export const Title = styled.h5`
+export const WrapperHeader = styled.section`
   grid-area: header_;
 
-  margin: ${metrics.space.small + 0.2}rem 0;
-  strong {
-    color: ${colors.quaternary};
+  display: flex;
+  justify-content: space-between;
+  margin: ${metrics.space.small}rem 0;
+  border-bottom: 1px dotted ${colors.tertiary};
+
+  div {
+    display: flex;
+    align-items: center;
+
+    a {
+      color: ${colors.primary};
+    }
+    h5 {
+      color: ${colors.quaternary};
+    }
+  }
+
+  div:last-child > div {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    margin-right: ${metrics.space.small}rem;
+    background: ${(props) =>
+      props.status === 'ativo' ? colors.green : colors.red};
+  }
+  div:last-child > strong {
+    color: ${(props) => (props.status === 'ativo' ? colors.green : colors.red)};
   }
 `;
 
 export const WrapperLocation = styled.section`
   grid-area: location;
+
   img {
     width: 100%;
     height: auto;
@@ -61,42 +87,37 @@ export const WrapperLocation = styled.section`
     border-radius: 8px;
     margin: ${metrics.space.small - 0.2}rem 0;
   }
+
   div {
-    margin: ${metrics.space.medium}rem 0;
+    border-bottom: 1px dotted ${colors.tertiary};
+    padding: ${metrics.space.small}rem 0;
+    line-height: ${metrics.space.bigger}rem;
   }
 `;
 
 export const WrapperImovel = styled.section`
   grid-area: info;
 
-  #info-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-    margin-bottom: ${metrics.space.smaller}rem;
-  }
-
   #cards-container {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-top: ${metrics.space.medium}rem;
     padding: ${metrics.space.medium}rem 0;
-    border-top: 1px solid ${colors.tertiary};
+    border-top: 1px dotted ${colors.tertiary};
+    border-bottom: 1px dotted ${colors.tertiary};
 
     .cards-content {
       width: 100%;
       height: auto;
 
-      margin: 0 ${metrics.space.smaller}rem;
+      margin-right: ${metrics.space.medium}rem;
       padding: ${metrics.space.medium}rem;
       border: 1px solid ${colors.tertiary};
       border-radius: 8px;
       color: ${colors.quaternary};
 
       div {
-        margin: 0;
-        padding: 0;
-        border: 0;
         display: flex;
         align-items: center;
 
@@ -107,59 +128,16 @@ export const WrapperImovel = styled.section`
       }
     }
   }
-
-  #beds-container {
-    ul {
-      display: flex;
-      justify-content: space-between;
-      border-top: 1px solid ${colors.tertiary};
-    }
-    ul li {
-      color: ${colors.quaternary};
-      margin: ${metrics.space.big}rem;
-      strong {
-        color: ${colors.primary};
-      }
-    }
-  }
 `;
 
-export const DivStyled = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+export const WrapperComforts = styled.section`
+  grid-area: comforts;
+  margin-bottom: ${metrics.space.big * 2}rem;
+  padding-top: ${metrics.space.small}rem;
 
-  margin-left: ${metrics.space.small}rem;
-  padding: ${metrics.space.smaller - 0.4}rem ${metrics.space.smaller - 0.1}rem;
-
-  div {
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    margin-right: 1rem;
-    background: ${(props) =>
-      props.status === 'active' ? colors.green : colors.red};
-  }
-
-  strong {
-    color: ${(props) =>
-      props.status === 'active' ? colors.green : colors.red};
-  }
-`;
-
-export const WrapperRules = styled.section`
-  grid-area: rules;
-
-  #rules-container {
-    display: flex;
-    justify-content: space-between;
-    border-top: 1px solid ${colors.tertiary};
-
-    ul li {
-      color: ${colors.quaternary};
-      margin: ${metrics.space.medium}rem 0;
-      list-style: none;
-    }
+  ul li {
+    display: inline-flex;
+    margin: ${metrics.space.medium}rem;
   }
 `;
 
@@ -168,25 +146,6 @@ export const StrongStyled = styled.strong`
   padding: ${metrics.space.smaller - 0.4}rem ${metrics.space.smaller - 0.1}rem;
   color: ${colors.secondary};
   background: ${(props) =>
-    props.status === 'sim' ? colors.green : colors.red};
+    props.hasComfort === 'sim' ? colors.green : colors.red};
   border-radius: 5px;
-`;
-
-export const WrapperComforts = styled.section`
-  grid-area: comforts;
-  margin: ${metrics.space.medium - 0.1}rem 0;
-
-  #comforts-container {
-    display: flex;
-    justify-content: space-between;
-    border-top: 1px solid ${colors.tertiary};
-
-    ul li {
-      color: ${colors.quaternary};
-      margin: ${metrics.space.medium}rem ${metrics.space.big}rem;
-    }
-  }
-  button {
-    margin-left: ${metrics.space.smaller - 0.4}rem;
-  }
 `;
